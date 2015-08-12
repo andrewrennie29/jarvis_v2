524935d02cee52b22fd21704bc7aea5c6c61cdb5
Andrew Rennie <andrew.rennie@me.com>
2015-08-12 07:31:36 +0100

    Almost working, need to finish custom update method

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -19,6 +19,7 @@ class StatusesController < ApplicationController
 
   # GET /statuses/1/edit
   def edit
+    @status.update(status_params)
   end
 
   # POST /statuses
@@ -41,7 +42,7 @@ class StatusesController < ApplicationController
   # PATCH/PUT /statuses/1.json
   def update
     respond_to do |format|
-      if @status.update(status_params)
+      if @status.update_status(status_params)
         format.html { redirect_to @status, notice: 'Status was successfully updated.' }
         format.json { render :show, status: :ok, location: @status }
       else
@@ -69,6 +70,6 @@ class StatusesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
-      params.require(:status).permit(:notstarted, :inprogress, :forreview, :delayed, :complete, :todo_id)
+      params.require(:status).permit(:id, :notstarted, :inprogress, :forreview, :delayed, :complete)
     end
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,8 @@
 class Status < ActiveRecord::Base
   belongs_to :todo
+  
+  def update_status(status_params)
+  	self.delayed = true
+  end
+
 end
